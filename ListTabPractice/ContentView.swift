c376b9d88b5c7ea515fbbcaf1cbaf23d2d7f0049
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-11-12 08:13:03 +0400

    Initial Commit

---
+++ ListTabPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListTabPractice
+//
+//  Created by Nadzeya Shpakouskaya on 12/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListTabPractice/ListTabPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTabPracticeApp.swift
+//  ListTabPractice
+//
+//  Created by Nadzeya Shpakouskaya on 12/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ListTabPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
